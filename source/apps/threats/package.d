eb753cc019093ae7dd99eafe5709b689eb506d1c
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:41 +0200

    Update

--- source/apps/threats/package.d
+++ source/apps/threats/package.d
@@ -1,7 +1,35 @@
 module apps.threats;
 
-public import apps.threats.controllers;
-public import apps.threats.routes;
-public import apps.threats.views;
+mixin(ImportPhobos!());
 
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.tht.controllers;
+  import apps.tht.helpers;
+  import apps.tht.tests;
+  import apps.tht.views;
+}
+
+DApp threatsApp;
+static this() {
+  threatsApp = App
+    .name("threatsApp")
+    .rootPath("/apps/threats")
+    .addRoute(Route("", HTTPMethod.GET, THTIndexPageController))
+    .addRoute(Route("/", HTTPMethod.GET, THTIndexPageController));
+}
 
